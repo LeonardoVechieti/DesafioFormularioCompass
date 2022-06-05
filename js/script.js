326a24fb9c648224cd59b0a4f38601825a5015e7
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-06-05 17:33:29 -0300

    develop success button logic

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//função para botao de retorno
+function back(){
+    document.getElementById("page-register").style.display="block";
+    document.getElementById("page-sucess").style.display="none";
+    return
+}
+//esconte o sucess ao carregar a pagina
+document.getElementById("page-sucess").style.display="none";
+
